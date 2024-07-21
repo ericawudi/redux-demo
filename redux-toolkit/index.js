02b37afce0bf15d91c10f1de9303a2432cfdd7d6
Awudi Eric Okyere <eoawudi@nalosolutions.com>
2024-07-21 01:18:26 +0000

    Redux-toolkit Async actions

--- redux-toolkit/index.js
+++ redux-toolkit/index.js
@@ -1,6 +1,7 @@
 const store = require("./app/store");
 const { cakeActions } = require("./features/cake/cakeSlice");
 const { iceCreamActions } = require("./features/icecream/iceCreamSlice");
+const { fetchUsers } = require("./features/user/userSlice");
 
 console.log("Initial state ", store.getState());
 
@@ -16,4 +17,5 @@ store.dispatch(iceCreamActions.ordered(2));
 store.dispatch(iceCreamActions.ordered(5));
 store.dispatch(iceCreamActions.restocked(5));
 store.dispatch(iceCreamActions.restocked(3));
-unsubsribe();
+store.dispatch(fetchUsers());
+// unsubsribe();
